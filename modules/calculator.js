168a4d5234a04aee6bcb0646c2f922e3ba535c64
Anand <s.anand.dubey@gmail.com>
2018-04-16 16:16:40 +1000

    remove object export
    it could be confusing

--- modules/calculator.js
+++ modules/calculator.js
@@ -1,15 +1,9 @@
 const PI180 = 0.0174533; // private to module, no way for other modules to import it.
 const logBase10 = (x) => Math.log10(x); // exported via default object ONLY
 
-const add = (x, y) => x + y; 
-const subtract = (x, y) => x - y;
+export const add = (x, y) => x + y; // can be exported specifically and as part of default object both
+export const subtract = (x, y) => x - y; // can be exported specifically and as part of default object both
 export const tangent = (degree) => Math.tan( PI180 * degree); // exported specifically only
-
-// exporting existing variables at one place
-export {
-    add, // can be exported specifically and as part of default object both
-    subtract, // can be exported specifically and as part of default object both
-};
 
 export default {
     add,
